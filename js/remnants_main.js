a8120773133c7873dd7ccf605169d293e1d65447
Matthew Davison <matthewdavison@Matthews-MacBook-Pro.local>
2019-04-24 23:02:15 -0400

    Figured out menus

--- js/remnants_main.js
+++ js/remnants_main.js
@@ -26,14 +26,14 @@ var controls;
 
 var game = new Phaser.Game(config);
 
-var content = ["You wake alone in a dark forest with no memory of your past…", "the stinging cold prompts you to quickly build a fire. Ahh the warmth… The wind quiets but the forest is still loud with noise. What is that rustling? There it is again. The RUSTLING. Louder now, it seems to be getting closer… and faster even. “Hello?” you yell. No answer… your fists tighten around the hatchet in your hands. Do you run? Or stand your ground?..."]
+var content = "You wake alone in a dark forest with no memory of your past… the stinging cold prompts you to quickly build a fire. Ahh the warmth… The wind quiets but the forest is still loud with noise. What is that rustling? There it is again. The RUSTLING. Louder now, it seems to be getting closer… and faster even. “Hello?” you yell. No answer… your fists tighten around the hatchet in your hands. Do you run? Or stand your ground?...";
 
 var line = [];
 
 var wordIndex = 0;
 var lineIndex = 0;
 
-var wordDelay = 120;
+var wordDelay = 70;
 var lineDelay = 400;
 
 var text;
@@ -47,12 +47,53 @@ function preload() {
     /* this.load.image('tiles', 'assets/images/grass_biome.png');
     this.load.tilemapTiledJSON('map', 'assets/tilemaps/map1_1.json');
     this.load.spritesheet('mantisplayer', 'assets/images/praying_mantis.png', { frameWidth: 128, frameHeight: 128 }); */
+    //this.load.image('button', 'assets/images/button.png'); //Temp
 }
 
 function create() {
-    var title = this.add.text(100, 200, 'Static Text Object', { fontFamily: 'Arial', fontSize: 64, color: '#00ff00' });
-    timerEvent = this.time.addEvent({delay: wordDelay, callback: typeText, repeat: testText.length});
-    text = this.add.text(100, 400);
+    //var title = this.add.text(100, 200, 'Static Text Object', { fontFamily: 'Arial', fontSize: 64, color: '#00ff00' });
+    testText = content;
+    //timerEvent = this.time.addEvent({delay: wordDelay, callback: typeText, repeat: testText.length});
+    //text = this.add.text(100, 400);
+
+    //Use containers to make buttons. Put bg image + text into a container, then put containers into one big menu container
+    //var buttonBG = this.add.image(0, 0, 'button');
+    var buttonText = this.add.text(0, 0, 'Option 1');
+    var buttonText2 = this.add.text(0, 200, 'Option 2');
+    buttonText.setInteractive();
+    buttonText2.setInteractive();
+
+    var menu = this.add.container(100, 100, [buttonText, buttonText2]);
+
+    buttonText.on('pointerover', function(){
+        buttonText.setTint(0x00ff00);
+    });
+
+    buttonText.on('pointerout', function(){
+        buttonText.clearTint();
+    });
+
+    buttonText.on('pointerup', function(){
+        buttonText.setText("Clicked");
+    });
+
+    buttonText2.on('pointerover', function(){
+        buttonText2.setTint(0x00ff00);
+    });
+
+    buttonText2.on('pointerout', function(){
+        buttonText2.clearTint();
+    });
+
+    buttonText2.on('pointerup', function(){
+        if(buttonText2.text == 'Clicked'){
+            buttonText2.setText("Option 2");
+        }
+        else{
+            buttonText2.setText("Clicked");
+        }
+    });
+
 }
 
 function moveToXY(gameObject, x, y, speed, maxTime) {
@@ -79,7 +120,9 @@ function update(time, delta) {
     
 }
 
+
 function typeText(){
     var currentText = testText.substring(0, testText.length - timerEvent.repeatCount);
     text.setText(currentText);
+    text.setWordWrapWidth(600, true);
 }
